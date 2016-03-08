42d24c0cd3e021357eb09a5d08d6d8fb8ba49a0f
Bruno <lapierre.bruno@gmail.com>
2016-03-08 01:39:43 +0100

    mise ajour du fichier side_nav et update de font awsome

--- resources/templates/back/side_nav.php
+++ resources/templates/back/side_nav.php
@@ -1,27 +1,87 @@
 	<div class="collapse navbar-collapse navbar-ex1-collapse">
 		<ul class="nav navbar-nav side-nav">
+            
+            
+            
+            <!-- Pour Users -->
+            <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#user"><i class="fa fa-fw fa-users"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="user" class="collapse">
+                            <li>
+                                <a href="index.php?prospects"><i class="fa fa-fw fa-user-plus"></i> Request Membership</a>
+                            </li>
+                            <li>
+                                <a href="index.php?members"><i class="fa fa-fw fa-user"></i> View Members</a>
+                            </li>
+                        </ul>
+            </li>
+            <!-- Pour Films -->
+            <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#film"><i class="fa fa-fw fa-film fa-lg"></i> Movies <i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="film" class="collapse">
+                            <li>
+                                <a href="index.php?movies"><i class="fa fa-fw fa-film"></i> View Movies</a>
+                            </li>
+                            <li>
+                                <a href="index.php?add_movie"><i class="fa fa-fw fa-plus-square"></i> Add a Movie</a>
+                            </li>
+                        </ul>
+            </li>
+            <!-- Pour Genre -->
+            <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#genre"><i class="fa fa-fw fa-tags fa-lg"></i> Genre<i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="genre" class="collapse">
+                            <li>
+                                <a href="index.php?genres"><i class="fa fa-fw fa-tags"></i> Genre</a>
+                            </li>
+                            <li>
+                                <a href="index.php?add_genre"><i class="fa fa-fw fa-plus-square"></i> Add a Genre</a>
+                            </li>
+                        </ul>
+            </li>
+            <!-- Pour Director -->
+            <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#director"><i class="fa fa-fw fa-video-camera fa-lg"></i> Directors<i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="director" class="collapse">
+                            <li>
+                                <a href="index.php?directors"><i class="fa fa-fw fa-video-camera"></i> View Directors</a>
+                            </li>
+                            <li>
+                                <a href="index.php?add_product"><i class="fa fa-fw fa-plus-square"></i> Add a Director</a>
+                            </li>
+                        </ul>
+            </li>
+            <!-- Pour Actor -->
+            <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#actor"><i class="fa fa-fw fa-star fa-lg"></i> Actors<i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="actor" class="collapse">
+                            <li>
+                                <a href="index.php?actors"><i class="fa fa-fw fa-star"></i> View Actors</a>
+                            </li>
+                            <li>
+                                <a href="index.php?add_actor"><i class="fa fa-fw fa-plus-square"></i> Add an Actor</a>
+                            </li>
+                        </ul>
+            </li>
+            <!-- Pour Admin -->
+            <li>
+            <a href="javascript:;" data-toggle="collapse" data-target="#admin"><i class="fa fa-fw fa-user fa-lg"></i> Admin<i class="fa fa-fw fa-caret-down"></i></a>
+                        <ul id="admin" class="collapse">
+                            <li>
+                                <a href="index.php?admins"><i class="fa fa-fw fa-user"></i> View Admin</a>
+                            </li>
+                            <li>
+                                <a href="index.php?add_actor"><i class="fa fa-fw fa-plus-square"></i> Add an Admin</a>
+                            </li>
+                        </ul>
+            </li>
+            <!-- Pour Orders -->
 			<li class="">
-				<a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+				<a href="index.php?#######"><i class="fa fa-fw fa-credit-card"></i> Orders</a>
 			</li>
+            <!-- Pour Reports -->
 			<li class="">
-				<a href="index.php?orders"><i class="fa fa-fw fa-credit-card"></i> Orders</a>
+				<a href="index.php?########"><i class="fa fa-fw fa-ticket"></i> Reports</a>
 			</li>
-			<li class="">
-				<a href="index.php?reports"><i class="fa fa-fw fa-flag"></i> Reports</a>
-			</li>
-			<li>
-				<a href="index.php?products"><i class="fa fa-fw fa-database"></i> View Products</a>
-			</li>
-			<li>
-				<a href="index.php?add_product"><i class="fa fa-fw fa-plus-square"></i> Add Product</a>
-			</li>
-			
-			<li>
-				<a href="index.php?categories"><i class="fa fa-fw fa-tags"></i> Categories</a>
-			</li>
-			<li>
-				<a href="index.php?users"><i class="fa fa-fw fa-user"></i> Users</a>
-			</li>
-		
 		</ul>
 	</div>
